88768c3745f08526cb040a1f8812cbdcd83407eb
EvanYun <dbstjdduswkd@naver.com>
2017-07-14 17:27:36 -0700

    upload_new_file

---
+++ Test1/app/src/main/java/comdbstjdduswkd/naver/httpblog/test1/MapActivity.java
@@ -0,0 +1,26 @@
+package comdbstjdduswkd.naver.httpblog.test1;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by USER on 2017-07-14.
+ */
+
+public class MapActivity extends Fragment {
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.map_layout, container, false);
+        return view;
+    }
+}
