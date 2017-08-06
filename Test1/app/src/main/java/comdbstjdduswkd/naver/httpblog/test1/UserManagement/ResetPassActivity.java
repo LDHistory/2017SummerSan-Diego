68c18b6dc833c2b89a354158ec3b831498f6bc86
LDHistory <ehd8266@naver.com>
2017-08-05 17:27:37 -0700

    Upload Usermanagement

--- Test1/app/src/main/java/comdbstjdduswkd/naver/httpblog/test1/UserManagement/ResetPassActivity.java
+++ Test1/app/src/main/java/comdbstjdduswkd/naver/httpblog/test1/UserManagement/ResetPassActivity.java
@@ -1,7 +1,6 @@
 package comdbstjdduswkd.naver.httpblog.test1.UserManagement;
 
 import android.content.Intent;
-import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,62 +20,54 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import comdbstjdduswkd.naver.httpblog.test1.MainActivity;
 import comdbstjdduswkd.naver.httpblog.test1.R;
 
-public class ResetActivity extends AppCompatActivity {
+public class ResetPassActivity extends AppCompatActivity {
 
-    private Button resetSend, resetCancel;
-    private TextView emailText;
-
-    String IDcheckResult;
+    private TextView CheckCode, NewPassword, ConfirmPass;
+    private Button Change;
+    private  String code, pass, SendResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reset);
+        setContentView(R.layout.activity_reset_pass);
 
-        //Check policy
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+        CheckCode = (TextView)findViewById(R.id.checkcode);
+        NewPassword = (TextView)findViewById(R.id.newpassword);
+        ConfirmPass = (TextView)findViewById(R.id.confirmpass);
 
-        //+++++++++++++++++++Button Object++++++++++++++++++++++
-        resetSend = (Button) findViewById(R.id.okbtn);
-        resetCancel = (Button) findViewById(R.id.resetCancel);
-        //++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        Change = (Button)findViewById(R.id.change);
 
-        //+++++++++++++++++++Button Object++++++++++++++++++++++
-        emailText = (TextView) findViewById(R.id.emailtext);
-        //++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-        //++++++++++++++++++++OnclickListner+++++++++++++++++++++++++
-        resetSend.setOnClickListener(new View.OnClickListener() {
+        Change.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent change = new Intent(ResetActivity.this, ResetPassActivity.class);
-                if(!HttpIDCheck(emailText.getText().toString())) {
-                    startActivity(change);
-                    finish();
-                }
+                code = CheckCode.getText().toString();
+                pass = NewPassword.getText().toString();
+                if (!CheckCode.getText().toString().equals("") && !NewPassword.getText().toString().equals("")
+                        && !ConfirmPass.getText().toString().equals("")) {
+                    if (NewPassword.getText().toString().equals(ConfirmPass.getText().toString())) {
+                        if (!HttpIDCheck()) {
+                            Intent main = new Intent(ResetPassActivity.this, LoginActivity.class);
+                            startActivity(main);
+                            Toast.makeText(ResetPassActivity.this, "Successfully change your password!", Toast.LENGTH_LONG).show();
+                            finish();
+                        }
+                    } else
+                        Toast.makeText(ResetPassActivity.this, "Please, Check your Confirm Password.", Toast.LENGTH_LONG).show();
+                }else
+                    Toast.makeText(ResetPassActivity.this, "Please, Write all information.", Toast.LENGTH_LONG).show();
             }
         });
-
-        resetCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     }
 
-    public boolean HttpIDCheck(String email) {
+    public boolean HttpIDCheck() {
         boolean check = false;
         try {
             //--------------------------
             //   URL 설정하고 접속하기
             //--------------------------
-            URL url = new URL("http://teamb-iot.calit2.net/slim-api/appreset-password");       // URL 설정
+            URL url = new URL("http://teamb-iot.calit2.net/slim-api/appreset-password-change");       // URL 설정
             HttpURLConnection http = (HttpURLConnection) url.openConnection();   // 접속
             //--------------------------
             //   전송 모드 설정 - 기본적인 설정이다
@@ -92,7 +83,8 @@ public class ResetActivity extends AppCompatActivity {
             //   서버로 값 전송 (URL Tag protocol)
             //--------------------------
             StringBuffer buffer = new StringBuffer();
-            buffer.append("email").append("=").append(email);                 // php 변수에 값 대입
+            buffer.append("token").append("=").append(code).append("&");                 // php 변수에 값 대입
+            buffer.append("password").append("=").append(pass);
 
             OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream(), "EUC-KR");
             PrintWriter writer = new PrintWriter(outStream);
@@ -108,14 +100,14 @@ public class ResetActivity extends AppCompatActivity {
             while ((str = reader.readLine()) != null) {       // 서버에서 라인단위로 보내줄 것이므로 라인단위로 읽는다
                 builder.append(str + "\n");                     // View에 표시하기 위해 라인 구분자 추가
             }
-            IDcheckResult = builder.toString();                       // 전송결과를 전역 변수에 저장
+            SendResult = builder.toString();                       // 전송결과를 전역 변수에 저장
             try {
-                JSONObject jsonObject = new JSONObject(IDcheckResult);
+                JSONObject jsonObject = new JSONObject(SendResult);
                 if (jsonObject.getString("status").equals("true")) {
                     check = true;
                 }else
-                    Toast.makeText(ResetActivity.this, "This email dosen't exist.", Toast.LENGTH_LONG).show();
-                    check = false;
+                    Toast.makeText(ResetPassActivity.this, "Please, Check your code and Password.", Toast.LENGTH_LONG).show();
+                check = false;
             } catch (JSONException e) {
                 e.printStackTrace();
             }
